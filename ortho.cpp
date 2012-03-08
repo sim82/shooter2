4709522d7f0a4b6f8f7383b5ea886f2049f1a34a
Simon Berger <simberger@gmail.com>
2012-03-08 21:12:01 +0100

    testing vertex buffer objects

--- ortho.cpp
+++ ortho.cpp
@@ -661,6 +661,8 @@ public:
             *(first++) = verts_[i].x;
             *(first++) = verts_[i].y;
             *(first++) = verts_[i].z;
+            
+            
         }
         
     }
@@ -1556,6 +1558,105 @@ private:
 
 };
 
+class vbo_builder {
+  
+    
+public:
+    vbo_builder( size_t num_planes ) : num_planes_(num_planes) {
+        GLuint b;
+        glGenBuffers( 2, buffers_ );
+        glGenBuffers( 1, &index_buffer_ );
+        
+        const size_t vertex_size = num_planes_ * (4 * 3) * sizeof(float);
+        glBindBuffer( GL_ARRAY_BUFFER, buffers_[0] );
+        glBufferData( GL_ARRAY_BUFFER, vertex_size, 0, GL_STATIC_DRAW );
+        
+        const size_t color_size = num_planes_ * (4 * 3) * sizeof(float);
+        glBindBuffer( GL_ARRAY_BUFFER, buffers_[1] );
+        glBufferData( GL_ARRAY_BUFFER, color_size, 0, GL_DYNAMIC_DRAW );
+        
+        glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, index_buffer_ );
+        glBufferData( GL_ELEMENT_ARRAY_BUFFER, num_planes_ * 4 * sizeof(int), 0, GL_STATIC_DRAW );
+        
+        
+    }
+    
+    void update_index_buffer( size_t num_planes ) {
+        glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, index_buffer_ );
+        GLuint *b = (GLuint *) glMapBuffer( GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY );
+        assert( b != nullptr );
+        for( size_t i = 0; i < num_planes * 4; ++i ) {
+            b[i] = i;
+        }
+        
+        glUnmapBuffer( GL_ELEMENT_ARRAY_BUFFER );
+        
+    }
+    
+    template<typename iiter>
+    void update_vertices( iiter first, iiter last ) {
+        //const size_t num_planes_ = std::distance( first, last );
+        
+        glBindBuffer( GL_ARRAY_BUFFER, buffers_[0] );
+        float *b = (float*) glMapBuffer( GL_ARRAY_BUFFER, GL_WRITE_ONLY );
+        assert( b != nullptr );
+        for( ; first != last; ++first ) {
+         
+            const plane &p = *first;
+            //size_t s1 = vertex_.size();
+            
+            p.render_vertex( b );
+            b += 4 * 3;
+            
+        }
+        
+        glUnmapBuffer( GL_ARRAY_BUFFER );
+    }
+    
+    template<typename iiter>
+    void update_color( iiter first, iiter last ) {
+//         const size_t num_planes_ = std::distance( first, last );
+        
+        glBindBuffer( GL_ARRAY_BUFFER, buffers_[1] );
+        float *b = (float*) glMapBuffer( GL_ARRAY_BUFFER, GL_WRITE_ONLY );
+        assert( b != nullptr );
+        
+        //b += 4 * 3 * num_planes_;
+        
+        for( ; first != last; ++first ) {
+            for( size_t i = 0; i < 4; ++i ) {
+                *(b++) = first->r;
+                *(b++) = first->g;
+                *(b++) = first->b;
+            }   
+            
+        }
+        
+        glUnmapBuffer( GL_ARRAY_BUFFER );
+    }
+    
+    void draw_arrays() {
+        glBindBuffer( GL_ARRAY_BUFFER, buffers_[0] );
+        glVertexPointer(3, GL_FLOAT, 0, (GLvoid*)((char*)NULL));
+       // glColorPointer(3, GL_FLOAT, 0, (GLvoid*)((char*)NULL+ 4 * 3 * num_planes_ * sizeof(GLfloat) ));
+        glBindBuffer( GL_ARRAY_BUFFER, buffers_[1] );
+        glColorPointer(3, GL_FLOAT, 0, (GLvoid*)((char*)NULL));
+        
+//         glBindBuffer(GL_ARRAY_BUFFER, buffer_);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_);
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glEnableClientState(GL_COLOR_ARRAY);
+// This is the actual draw command
+        glDrawElements(GL_QUADS, num_planes_ * 4, GL_UNSIGNED_INT, (GLvoid*)((char*)NULL));
+    }
+private:
+    
+    GLuint buffers_[2];
+    GLuint index_buffer_;
+    
+    const size_t num_planes_;
+};
+
 class vertex_array_builder {
 public:
     vertex_array_builder() {
@@ -1994,6 +2095,9 @@ public:
         vertex_array_builder vab;
         vab.render(planes_.begin(), planes_.end() );
         
+        vbo_builder vbob(planes_.size());
+        vbob.update_index_buffer(planes_.size());
+        vbob.update_vertices( planes_.begin(), planes_.end());
         while ( true ) {
 
             //cube c(x1, 0, y1);
@@ -2085,14 +2189,14 @@ public:
 
 
 //          glPushMatrix();
-
+            
             //CL_Mat4f proj = CL_Mat4f::look_at( 20, 20, 20, 0, 0, 0, 0.0, 1.0, 0.0 );
 
             //vab.update_color( planes_.begin(), planes_.end() );
-            vab.update_color( ls.rad_rgb().begin(), ls.rad_rgb().end() );
+            //vab.update_color( ls.rad_rgb().begin(), ls.rad_rgb().end() );
             
-            vab.setup_gl_pointers();
-            
+            //vab.setup_gl_pointers();
+            vbob.update_color(ls.rad_rgb().begin(), ls.rad_rgb().end());
             //setup_perspective( p1 );
             setup_ortho();
 
@@ -2106,8 +2210,8 @@ public:
             glViewport( 0, 0, gc.get_width(), gc.get_height());
             //render_quads();
            // vab.setup_gl_pointers();
-            vab.draw_arrays();
-
+            //vab.draw_arrays();
+            vbob.draw_arrays();
 
             wnd_.flip(1);
 
